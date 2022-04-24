abfcc0e5f96167a5a11d3269617dab4171a753de
unknown <spirited.pentest@gmail.com>
2022-04-24 11:21:18 +0200

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TouchableOpacity} from 'react-native';
+import {useState} from 'react';
+
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'shaun', id:'1'},
+    { name: 'yoshi', id:'2'},
+    { name: 'marion', id:'3'},
+    { name: 'john', id:'4'},
+    { name: 'xad', id:'5'},
+    { name: 'fan', id:'6'},
+    { name: 'ted', id:'7'}
+    
+  ])
+  
+  const pressHandler = (id) =>{
+      console.log(` the id  : ${id}`);
+      setPeople((prevPeople)=>{
+        return prevPeople.filter(person=> person.id != id)
+      })
+  }
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item)=> item.id}
+        data= {people}
+        renderItem = {({item})=>(
+          <View>
+           <TouchableOpacity onPress={()=>pressHandler(item.id)}>
+             <Text style={styles.name} >{item.name}</Text>
+           </TouchableOpacity>      
+          </View>
+        )}
+      />
+      { /*people.map((item, key)=>
+        (
+          <View key={item.key}>
+            <Text >{item.name}</Text>
+            </View>
+        )
+      })*/}
+
     </View>
+  
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +58,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  name:{
+    backgroundColor:"orange",
+    marginVertical: 3,
+    fontSize:25,
+    color: 'white',
+    padding: 5,
+    textAlign:'center',
+    marginHorizontal: 5,
+    marginTop: 24,
+    
+ 
+
+  }
 });
